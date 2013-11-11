57989cc274db20b00dbe60054d880a15177ff5e7
Danesh Mondegarian <daneshm90@gmail.com>
2013-11-10 23:30:56 -0800

    SystemUI : Port forward statusbar brightness control
    Brings back the status bar brightness control from cm-10.2/cm-11
    
    Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    SystemUI: improve brightness slider behavior
    
    Mimic the brightness slider behavior in the statusbar.
    This adds logic to make the statusbar slider also work with automatic
    brightness mode enabled and it will instead adjust the temporary automatic
    brightness overrride.
    
    Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: status bar brightness: store value as int
    
    Non-automatic brightness value is stored as int, not float.
    
    Symptom: adjust the brightness in the status bar, the brightness
             slider in the notification bar will always be set to full

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3866,6 +3866,12 @@ public final class Settings {
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_LOCK_SCREEN = "double_tap_sleep_lock_screen";
+
+        /**
+         * Whether to control brightness from status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
       
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
