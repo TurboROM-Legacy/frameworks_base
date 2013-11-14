f59dcdc91e5d85d6f2d27c8de397fc8cc63248fc
Lars Greiss <kufikugel@googlemail.com>
2013-11-14 15:47:19 +0100

    Initial bringup of Slim action resources
    This also includes:  Frameworks: add ability to send a custom intent after keyguard is finished
    
    I, @xX-Yoshi-Xx, have modified this commit some to add compatibility for future nav bar customization. Thus, it includes a few nav bar items that kufikugel did not
    originally add.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1080,6 +1080,27 @@ public abstract class BaseStatusBar extends SystemUI implements
         mHandler.sendEmptyMessage(msg);
     }
 
+    @Override
+    public void toggleLastApp() {
+        int msg = MSG_TOGGLE_LAST_APP;
+        mHandler.removeMessages(msg);
+        mHandler.sendEmptyMessage(msg);
+    }
+
+    @Override
+    public void toggleKillApp() {
+        int msg = MSG_TOGGLE_KILL_APP;
+        mHandler.removeMessages(msg);
+        mHandler.sendEmptyMessage(msg);
+    }
+
+    @Override
+    public void toggleScreenshot() {
+        int msg = MSG_TOGGLE_SCREENSHOT;
+        mHandler.removeMessages(msg);
+        mHandler.sendEmptyMessage(msg);
+    }
+
     protected H createHandler() {
          return new H();
     }
