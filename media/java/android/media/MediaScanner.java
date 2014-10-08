8a164c6569a979f1ccccb1a8373320ddc21f5194
Bartosz Bialek <bartosz.bialek@sonymobile.com>
2014-10-08 13:31:34 +0200

    Do not scan a file which cannot be read
    When the media scanner is invoked to scan a file which it cannot access
    (e.g. one in the app-private directories) the related database entry
    will be created anyway, which doesn't make sense. This results in having
    broken entries showed up in e.g. Album app.
    
    This fix is to prevent scanning files which are inaccessible.
    
    Change-Id: I5b4909bf709c82d66e891f3e7f6890febc90c6eb
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- media/java/android/media/MediaScanner.java
+++ media/java/android/media/MediaScanner.java
@@ -1391,7 +1391,7 @@ public class MediaScanner
             prescan(path, true);
 
             File file = new File(path);
-            if (!file.exists()) {
+            if (!file.exists() || !file.canRead()) {
                 return null;
             }
 
