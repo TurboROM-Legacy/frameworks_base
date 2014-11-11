2a7f96660cb8aed1f0b6c03edc58444544544994
Danny Baumann <dannybaumann@web.de>
2014-11-11 13:13:08 +0100

    Add back increasing ring feature (1/3)
    Change-Id: I76c22ea94f25c576972361ad0ab209accb8ae1de
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	media/java/android/media/IRingtonePlayer.aidl
    	packages/SystemUI/src/com/android/systemui/media/RingtonePlayer.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4747,6 +4747,24 @@ public final class Settings {
         public static final String STATUS_BAR_NETWORK_ICONS_AIRPLANE_MODE_COLOR_DARK_MODE = "status_bar_network_icons_airplane_mode_color_dark_mode";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4807,7 +4825,10 @@ public final class Settings {
             PHONE_BLACKLIST_NOTIFY_ENABLED,
             PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
             PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE,
-            PHONE_BLACKLIST_REGEX_ENABLED
+            PHONE_BLACKLIST_REGEX_ENABLED,
+            INCREASING_RING,
+            INCREASING_RING_START_VOLUME,
+            INCREASING_RING_RAMP_UP_TIME
         };
 
         /**
