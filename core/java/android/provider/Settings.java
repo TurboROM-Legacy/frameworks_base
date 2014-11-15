ea891e1e01ad1b03719672b5111122ba5a7ac9b2
Sam Mortimer <sam@mortimer.me.uk>
2014-11-14 18:15:16 -0800

    [1/2] frameworks/base: Privacy Guard
    PS2: * add missing call to updatePrivacyGuardNotificationLocked()
         * remove ic_privacy_guard and ic_privacy_guard_off
         * use stat_notify_privacy_guard drawable from Björn
         * rebase
    
    PS3: * exclude MODE_IGNORED from causing
           getPrivacyGuardSettingForPackage() returning true.
           multiuser restrictions on calls/sms otherwise
           cause privacyguard to appear to be on when really
           it isn't.
         * rebase
    
    Pulled together from:
    
    Author: Danesh M <daneshm90@gmail.com>
    Date:   Thu Sep 5 19:23:15 2013 -0700
    Privacy Guard : Reimplement backed by AppOps
    Re-implements privacy guard to use appops.
    Change-Id: I2b52e8cf0d6e126fb156f83df885b5551503cd9d
    
    Author: Jorge Ruesga <jorge@ruesga.com>
    Date:   Sat Jan 4 01:34:39 2014 +0100
    privacyguard: Share privacy package name between activity stacks
    AOSP 4.4 now has a stack supervisor with differents activity stacks for home and focus activities.
    Every ActivityStack instance has a ref to this supervisor. Just share the privacy guard package name
    throught the supervisor.
    Patchset 2: Fix formatting
    Change-Id: I5dd75cfa7f5e8b568a4302fce98809ae307c6d52
    JIRA: CYAN-2874
    Issue: https://jira.cyanogenmod.org/browse/CYAN-2874
    
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Sun Feb 23 19:23:31 2014 +0100
    Use MODE_ASK as the default ops mode in Privacy Guard
    * Prompting the user is far more useful as a default behavior. The
      choice will be remembered after the first request.
    Change-Id: Ie62f932047f8b761bd0dd9f836bcd1132062edf5
    
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Tue Feb 25 13:11:12 2014 +0100
    Relocate and cleanup new app ops features and PGify it
    Change-Id: I4eb65f6f88c2f2aecdf054ffd52c31c4d5866900
    
    Change-Id: I2a69d59f288630fefcc83e6109ab608fa28eb71b
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6067,6 +6067,12 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         * Whether newly installed apps should run with Privacy Guard by default
+         * @hide
+         */
+        public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -6123,6 +6129,7 @@ public final class Settings {
             SLEEP_TIMEOUT,
             DOUBLE_TAP_TO_WAKE,
             CAMERA_GESTURE_DISABLED,
+            PRIVACY_GUARD_DEFAULT,
         };
 
         /**
