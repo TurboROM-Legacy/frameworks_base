7a0c739b057a2984c4ee6d713ea883f2a4897c78
rascarlo <rascarlo@gmail.com>
2014-11-19 13:33:12 +0100

    Marshmallow: volume rocker wake (1/2)
    Commit by rascarlo, adapted for m by @KreAch3R

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3607,6 +3607,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_CONFIG = "navigation_bar_config";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
