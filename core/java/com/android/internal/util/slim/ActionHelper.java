4f58843f19061ad9aa5a03b62c90992246f7b686
Lars Greiss <kufikugel@googlemail.com>
2014-11-21 17:42:55 +0100

    [SQUASH] Frameworks: Keyguard - Add user configurable shortcuts
    Frameworks: Add several Slim helper classes and constants back
    
    Frameworks: smart pulldown we do not support anymore
    
    No QS nothing smart needed anymore
    
    Frameworks: inital bringup of slim actions and helper classes
    
    Bring back slimactions.
    
    - Most actions are available and adapted to LP (mainly statusbar actions we did on kk need to be ported back again)
    - Cleanup of old code
    - Renamed classes and methods
    
    and more.....
    
    This is the base for all the coming features now
    
    Frameworks: add all slim shortcut image resources
    
    Importing for now the old one from KitKat. Graphics group is currently
    vectorizing and revamping all. When they are finished we overwrite them and
    add xxxhdpi ones as well.
    
    Doing this now due that it is stopping the progress on a bunch of features right now
    
    And a small method renaming to be inline with the other methods
    
    Frameworks: add shortcut action icons
    
    Frameworks: Keyguard - Add user configurable shortcuts
    
    Change-Id: I32f86a067b6d54af32a1845cb1741ee55142d5d4

--- core/java/com/android/internal/util/slim/ActionHelper.java
+++ core/java/com/android/internal/util/slim/ActionHelper.java
@@ -78,6 +78,32 @@ public class ActionHelper {
                     config);
     }
 
+    // get and set the lockcreen shortcut configs from provider and return propper arraylist objects
+    // @ActionConfig
+    public static ArrayList<ActionConfig> getLockscreenShortcutConfig(Context context) {
+        String config = Settings.System.getStringForUser(
+                    context.getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS,
+                    UserHandle.USER_CURRENT);
+        if (config == null) {
+            config = "";
+        }
+
+        return (ConfigSplitHelper.getActionConfigValues(context, config, null, null, true));
+    }
+
+    public static void setLockscreenShortcutConfig(Context context,
+            ArrayList<ActionConfig> actionConfig, boolean reset) {
+        String config;
+        if (reset) {
+            config = "";
+        } else {
+            config = ConfigSplitHelper.setActionConfig(actionConfig, true);
+        }
+        Settings.System.putString(context.getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS, config);
+    }
+
     // General methods to retrieve the correct icon for the respective action.
     public static Drawable getActionIconImage(Context context,
             String clickAction, String customIcon) {
