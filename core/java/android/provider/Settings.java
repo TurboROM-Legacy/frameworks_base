f37794e9352ed5194f62443a5f9d921b4cf6ec4d
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters
     * Port from CM11
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4201,6 +4201,12 @@ public final class Settings {
         public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_regex_enabled";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
