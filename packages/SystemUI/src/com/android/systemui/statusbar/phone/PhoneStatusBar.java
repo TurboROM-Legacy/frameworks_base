a6353eed6d4e9e353cd9fdaa0e8f319a2460827e
Clark Scheff <clark@cyngn.com>
2015-01-16 10:31:15 -0800

    Themes: Call startKeyguard() when recreating status bar
    mStatusBarKeyguardViewManager holds on to a reference of ScrimController
    and we need to update this otherwise the scrim will remain on the screen
    and consume all touch events.
    
    This is easily reproduced by changing a theme and then turning the screen
    off and on to display the keyguard.  Once the keyguard is dismissed the
    scrim remains on the screen.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: Ie050a951117ed4aebef4ea3343366b9b9361b8a6
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3538,8 +3538,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mRecreating = false;
             }
         });
-        // restart the keyguard so it picks up the newly created ScrimController
-        startKeyguard();
 
         // if the keyguard was showing while this change occurred we'll need to do some extra work
         if (mState == StatusBarState.KEYGUARD) {
@@ -3550,6 +3548,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mIconController.hideNotificationIconArea(false);
             mIconController.hideSystemIconArea(false);
         }
+
+        // restart the keyguard so it picks up the newly created ScrimController
+        startKeyguard();
     }
 
     private void removeAllViews(ViewGroup parent) {
