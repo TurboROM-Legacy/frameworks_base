c1d67afde423a90b9d87f36d835f4d04515e0b32
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    SystemUI: Lock screen shortcut customization

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6488,6 +6488,12 @@ public final class Settings {
         public static final String DOZE_NOTIFICATION_INVERT_ENABLED = "doze_notification_invert_enabled";
 
         /**
+         * Launch actions for left/right lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
