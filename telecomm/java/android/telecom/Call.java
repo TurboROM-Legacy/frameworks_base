73d63ca79c701ca32bf9fb0c7ae504745cc08f5c
emancebo <emancebo@cyngn.com>
2015-02-10 15:17:18 -0800

    Expose call creation time to link call recordings with recent call log (1/2)
    Change-Id: I9743abbf23b767cf5cf1f27fffccb0f76618e5b1

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -278,6 +278,7 @@ public final class Call {
         private final int mCallCapabilities;
         private final int mCallProperties;
         private final DisconnectCause mDisconnectCause;
+        private final long mCreateTimeMillis;
         private final long mConnectTimeMillis;
         private final GatewayInfo mGatewayInfo;
         private final int mVideoState;
@@ -501,6 +502,13 @@ public final class Call {
         }
 
         /**
+         * @return the time the Call object was created
+         */
+        public long getCreateTimeMillis() {
+            return mCreateTimeMillis;
+        }
+
+        /**
          * @return Information about any calling gateway the {@code Call} may be using.
          */
         public GatewayInfo getGatewayInfo() {
@@ -550,6 +558,7 @@ public final class Call {
                         Objects.equals(mCallCapabilities, d.mCallCapabilities) &&
                         Objects.equals(mCallProperties, d.mCallProperties) &&
                         Objects.equals(mDisconnectCause, d.mDisconnectCause) &&
+                        Objects.equals(mCreateTimeMillis, d.mCreateTimeMillis) &&
                         Objects.equals(mConnectTimeMillis, d.mConnectTimeMillis) &&
                         Objects.equals(mGatewayInfo, d.mGatewayInfo) &&
                         Objects.equals(mVideoState, d.mVideoState) &&
@@ -571,6 +580,7 @@ public final class Call {
                     Objects.hashCode(mCallCapabilities) +
                     Objects.hashCode(mCallProperties) +
                     Objects.hashCode(mDisconnectCause) +
+                    Objects.hashCode(mCreateTimeMillis) +
                     Objects.hashCode(mConnectTimeMillis) +
                     Objects.hashCode(mGatewayInfo) +
                     Objects.hashCode(mVideoState) +
@@ -589,6 +599,7 @@ public final class Call {
                 int capabilities,
                 int properties,
                 DisconnectCause disconnectCause,
+                long createTimeMillis,
                 long connectTimeMillis,
                 GatewayInfo gatewayInfo,
                 int videoState,
@@ -603,6 +614,7 @@ public final class Call {
             mCallCapabilities = capabilities;
             mCallProperties = properties;
             mDisconnectCause = disconnectCause;
+            mCreateTimeMillis = createTimeMillis;
             mConnectTimeMillis = connectTimeMillis;
             mGatewayInfo = gatewayInfo;
             mVideoState = videoState;
@@ -1078,6 +1090,7 @@ public final class Call {
                 parcelableCall.getCapabilities(),
                 parcelableCall.getProperties(),
                 parcelableCall.getDisconnectCause(),
+                parcelableCall.getCreateTimeMillis(),
                 parcelableCall.getConnectTimeMillis(),
                 parcelableCall.getGatewayInfo(),
                 parcelableCall.getVideoState(),
