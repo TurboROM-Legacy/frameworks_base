7bb35790eaa14d76bf2a5672e6f0f905affd33a3
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    AICPfy
    increase top margin
    
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    Signed-off-by: DHOMD <DHO@dho.im>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4008,6 +4008,12 @@ public final class Settings {
          */
         public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
 
+      	/**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
