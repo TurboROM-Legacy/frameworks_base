daf2e1c86f575f6639553b702aa820d0b6084af4
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile (1/2)
    PureNexus Edits:
    adapt for qs tuner
    fix wrong logger on ime tile
    longclick for app notification settings

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -218,6 +218,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("compass")) return R.string.quick_settings_compass_label;
         else if (spec.equals("ime")) return R.string.quick_settings_ime_label;
         else if (spec.equals("volume")) return R.string.quick_settings_volume_panel_label;
+        else if (spec.equals("heads_up")) return R.string.quick_settings_heads_up_label;
         return 0;
     }
 
@@ -473,6 +474,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("compass")) return R.drawable.ic_qs_compass_on;
             else if (mSpec.equals("ime")) return R.drawable.ic_qs_ime;
             else if (mSpec.equals("volume")) return R.drawable.ic_qs_volume_panel;
+            else if (mSpec.equals("heads_up")) return R.drawable.ic_qs_heads_up_on;
             return R.drawable.android;
         }
 
