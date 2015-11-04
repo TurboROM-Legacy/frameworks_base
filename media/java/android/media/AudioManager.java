3c24672d69d514dc273c07c598873f71d9c2194b
maxwen <max.weninger@windriver.com>
2015-11-04 15:20:21 +0100

    base: rotation based volume key swap
    volume key routing is different then in LP
     and fix initd metrics
    Change-Id: I190bf07fd89fa1688244a399df731cf33af06f43
    
    Conflicts:
    	media/java/android/media/AudioManager.java

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -26,6 +26,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RemoteController.OnClientUpdateListener;
 import android.media.audiopolicy.AudioPolicy;
 import android.media.session.MediaController;
 import android.media.session.MediaSession;
