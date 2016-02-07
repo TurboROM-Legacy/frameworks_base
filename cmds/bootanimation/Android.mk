659ddb2f854858c7dabd3886d27a61e9e354331e
Raleigh Matlock <raleighman2@gmail.com>
2016-02-06 19:28:56 -0500

    Fix BootAnimation

--- cmds/bootanimation/Android.mk
+++ cmds/bootanimation/Android.mk
@@ -23,9 +23,7 @@ LOCAL_SHARED_LIBRARIES := \
     libEGL \
     libGLESv1_CM \
     libgui \
-    libtinyalsa \
-    libmedia \
-    libregionalization
+    libtinyalsa
 
 LOCAL_MODULE:= bootanimation
 
