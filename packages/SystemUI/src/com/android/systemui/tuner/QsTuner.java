0f13ebd6b91ba16f252b0e586f8f35e180a2773a
Alex Cruz <mazdarider23@gmail.com>
2016-02-12 21:44:17 -0500

    Add IME selector QS Tile
    See image - http://i.imgur.com/83vHFI0.png
    
    Change-Id: I48fd585901cecd24b0ba94409e69b6a5cda907b1

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -216,6 +216,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("expanded_desktop")) return R.string.quick_settings_expanded_desktop;
         else if (spec.equals("adb_network")) return R.string.quick_settings_adb_network;
         else if (spec.equals("compass")) return R.string.quick_settings_compass_label;
+        else if (spec.equals("ime")) return R.string.quick_settings_ime_label;
         return 0;
     }
 
@@ -469,6 +470,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("expanded_desktop")) return R.drawable.ic_qs_expanded_desktop;
             else if (mSpec.equals("adb_network")) return R.drawable.ic_qs_network_adb_on;
             else if (mSpec.equals("compass")) return R.drawable.ic_qs_compass_on;
+            else if (mSpec.equals("ime")) return R.drawable.ic_qs_ime;
             return R.drawable.android;
         }
 
