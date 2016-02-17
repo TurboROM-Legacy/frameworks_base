1841f0b74467310a15a43ccc03a0c09df6113478
cretin45 <cretin45@gmail.com>
2016-02-17 11:57:52 -0800

    SystemUI: Prevent search bar flicker between recents and launcher
    Issue-id: CYNGNOS-1892
    
    Change-Id: I200ccc319e81762c0485f405279bcc9880f07b71
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -216,10 +216,8 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                 Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
         mFinishLaunchHomeRunnable = new FinishRecentsRunnable(homeIntent,
             ActivityOptions.makeCustomAnimation(this,
-                mConfig.launchedFromSearchHome ? R.anim.recents_to_search_launcher_enter :
-                        R.anim.recents_to_launcher_enter,
-                    mConfig.launchedFromSearchHome ? R.anim.recents_to_search_launcher_exit :
-                        R.anim.recents_to_launcher_exit));
+                 R.anim.recents_to_search_launcher_enter,
+                    R.anim.recents_to_search_launcher_exit));
 
         // Mark the task that is the launch target
         int taskStackCount = stacks.size();
@@ -439,6 +437,14 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         if (!mConfig.launchedHasConfigurationChanged) {
             mRecentsView.disableLayersForOneFrame();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        if (mConfig.searchBarEnabled && mConfig.launchedFromHome) {
+            overridePendingTransition(0, 0);
+        }
+        super.onResume();
     }
 
     @Override
