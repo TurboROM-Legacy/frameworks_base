454ab4216f9178d56f12ec18fdb7018b125bc139
Randall Rushing <randall.rushing@gmail.com>
2016-03-25 22:20:06 -0700

    base: API to expose mNavigationBarCanMove from window manager [1/3]
    We need to ensure the views that NavigationController inflate
    are actually in line with the window behavior. It appears that
    when custom dpi is set, window manager gets it right, but
    NavigationController doesn't.
    
    Change-Id: Idf4472ae3ff8de372c44cb7a370f21d125248ec6
    
    Conflicts:
    	services/core/java/com/android/server/wm/WindowManagerService.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -8042,6 +8042,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     @Override
+    public boolean navigationBarCanMove() {
+        return mNavigationBarCanMove;
+    }
+    
+    @Override
     public void setLastInputMethodWindowLw(WindowState ime, WindowState target) {
         mLastInputMethodWindow = ime;
         mLastInputMethodTargetWindow = target;
