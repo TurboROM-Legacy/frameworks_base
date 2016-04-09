2eb177f2569c30d0652d416f06a6689a99827a09
mydongistiny <jaysonedson@gmail.com>
2016-04-09 17:07:58 -0400

    Add QS tile edit icon to expanded header (1/2)
    Brought to Turbo ROM by blinga6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4922,6 +4922,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
 
         /**
+         * Enable or disable the Quick Settings Edit Icon
+         * @hide
+         */
+        public static final String ENABLE_QS_EDIT_BUTTON = "quick_settings_edit_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
