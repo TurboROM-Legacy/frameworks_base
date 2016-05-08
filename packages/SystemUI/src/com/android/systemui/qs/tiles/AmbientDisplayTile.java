52f4cc2a86bb075797e428ccc955a17be95edb46
Randall Rushing <randall.rushing@gmail.com>
2016-05-08 00:05:24 -0700

    Tiles: Remove unused observer in AmbientDisplayTile
    The object in question is instantiated but setListening is
    never invoked on it. The target URI is already observed making
    this object redundant.
    
    Change-Id: I0eef2c36e4138be852af0741201016dd3e753c96
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
@@ -16,32 +16,20 @@
 
 package com.android.systemui.qs.tiles;
 
-import android.content.Context;
 import android.content.Intent;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.provider.Settings;
-import android.provider.Settings.Secure;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.qs.SecureSetting;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.R;
 
 /** Quick settings tile: Ambient Display **/
 public class AmbientDisplayTile extends QSTile<QSTile.BooleanState> {
 
-    private final SecureSetting mSetting;
-
     public AmbientDisplayTile(Host host) {
         super(host);
-
-        mSetting = new SecureSetting(mContext, mHandler, Secure.DOZE_ENABLED) {
-            @Override
-            protected void handleValueChanged(int value, boolean observedChange) {
-                handleRefreshState(value);
-            }
-        };
     }
 
     @Override
