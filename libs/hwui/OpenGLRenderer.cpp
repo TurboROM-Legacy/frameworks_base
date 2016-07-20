687cf60b6751565bc456fa496e4ee47b4a041cad
Raleigh Matlock <raleighman2@gmail.com>
2016-07-19 20:43:48 -0400

    libhwui: squashed updates
    * This includes:
    
    Don't unregister Fonts from renderer at destruction
    Author: Chris Craik <ccraik@google.com>
    
    Fix memory corruption in SpotShadow
    Author: Keith Mok <kmok@cyngn.com>
    
    Fix memory leak in HWUI
    Author: Sangkyu Lee <geteuid@gmail.com>
    
    libhwui: make setSurface asynchronous
    Author: Thomas Buhot <thomas.buhot@intel.com>
    
    hwui: Allow devices to opt-out of asynchronouse setSurface initialization
    Author: Christopher R. Palmer <crpalmer@gmail.com>
    
    fix race condition between HWUI cache and renderThread
    Author: Thomas Buhot <thomas.buhot@intel.com>
    
    libhwui: fix texture memory leak
    Author: Dohyun Lee <leedhyun11@gmail.com>
    
    Performance Optimization: Align texture dirty rect
    Author: ywen <ywen@codeaurora.org>
    
    Remove opaque check in preparing dirty region for all QCOM device
    Author: dianlujitao <dianlujitao@gmail.com>
    
    libhwui: Remove opaque check for everything
    Author: Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
    
    Fix random crash in libhwui
    Author: Keith Mok <kmok@cyngn.com>
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -181,15 +181,14 @@ void OpenGLRenderer::discardFramebuffer(float left, float top, float right, floa
 }
 
 void OpenGLRenderer::clear(float left, float top, float right, float bottom, bool opaque) {
-    if (!opaque) {
-        mRenderState.scissor().setEnabled(true);
-        mRenderState.scissor().set(left, getViewportHeight() - bottom, right - left, bottom - top);
-        glClear(GL_COLOR_BUFFER_BIT);
-        mDirty = true;
+    mRenderState.scissor().setEnabled(true);
+    mRenderState.scissor().set(left, getViewportHeight() - bottom, right - left, bottom - top);
+    glClear(GL_COLOR_BUFFER_BIT);
+    mDirty = true;
+    if (opaque) {
+        mRenderState.scissor().reset();
         return;
     }
-
-    mRenderState.scissor().reset();
 }
 
 void OpenGLRenderer::startTilingCurrentClip(bool opaque, bool expand) {
@@ -1699,6 +1698,7 @@ void OpenGLRenderer::drawPatch(const SkBitmap* bitmap, const Patch* mesh,
 
     Texture* texture = entry ? entry->texture : mCaches.textureCache.get(bitmap);
     if (!texture) return;
+    const AutoTexture autoCleanup(texture);
 
     // 9 patches are built for stretching - always filter
     int textureFillFlags = TextureFillFlags::ForceFilter;
